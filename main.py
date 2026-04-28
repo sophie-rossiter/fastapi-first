c72904341b6f551ce589b183a789a9cc28fc3c47
sophie-rossiter <sophrossit@icloud.com>
2026-04-28 19:00:50 +0100

    Add task list API with POST and GET endpoints

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Task(BaseModel):
+    title: str
+    done: bool | None = False
+
+tasks = []
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.post("/tasks/")
+def add_task(task: Task):
+    tasks.append(task)
+    return task
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int):
-    return {"item_id": item_id}
+@app.get("/tasks")
+def get_tasks():
+    return tasks
+
+@app.get("/tasks/{id}")
+def get_task(id: int):
+    current_task = tasks[id]
+    return current_task
