9766cf84be25c23402bb76fc603b01a7645097e6
sophie-rossiter <sophrossit@icloud.com>
2026-04-30 21:07:57 +0100

    Added docstrings to functions

--- main.py
+++ main.py
@@ -11,14 +11,17 @@ app = FastAPI()
 
 @app.post("/tasks/")
 def add_task(task: Task):
+    '''Accepts a Task in the request body, appends it to the task list, and returns the created task'''
     tasks.append(task)
     return task
 
 @app.get("/tasks")
 def get_tasks():
+    """Responds to a GET request to /tasks by returning the tasks list"""
     return tasks
 
 @app.get("/tasks/{id}")
 def get_task(id: int):
+    """Takes the task ID from the URL path, retrieves the corrosponding task from the task list and returns it"""
     current_task = tasks[id]
     return current_task
